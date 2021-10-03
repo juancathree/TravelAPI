36f1a95466fe4a91a33cf4b8063a665c79ec33f4
juancathree <juancarloslucenamonje@gmail.com>
2021-10-03 03:06:01 +0100

    add place router

--- server/router.go
+++ server/router.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	placeRouter "TravelAPI/pkg/place/infrastructure/router"
 	travelRouter "TravelAPI/pkg/travel/infrastructure/router"
 	"TravelAPI/pkg/user/infrastructure/middleware"
 	authRouter "TravelAPI/pkg/user/infrastructure/router"
@@ -14,4 +15,8 @@ func SetupRouter() {
 	// Travel
 	travel := app.Group("/travel", middleware.AuthRequired())
 	travelRouter.Router(travel)
+
+	// Place
+	place := app.Group("/place", middleware.AuthRequired())
+	placeRouter.Router(place)
 }
