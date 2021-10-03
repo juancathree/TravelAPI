c628cc26a097ed02a809617bb5278e036b2c0664
juancathree <juancarloslucenamonje@gmail.com>
2021-10-03 03:10:24 +0100

    add validation

--- pkg/user/application/Register.go
+++ pkg/user/application/Register.go
@@ -1,8 +1,18 @@
 package application
 
-import "TravelAPI/pkg/user/domain"
+import (
+	"TravelAPI/pkg/user/domain"
+	"TravelAPI/shared"
+	"fmt"
+)
 
 func Register(user *domain.User) (*domain.User, error) {
+
+	err := shared.Validate.Struct(user)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
 	// Set password
 	user.SetPassword(&user.Password)
 
