d479b181db0ba082aa0dca9ef1877290f663f8e7
juancathree <juancarloslucenamonje@gmail.com>
2021-10-03 03:07:55 +0100

    update methods

--- pkg/travel/domain/Repository.go
+++ pkg/travel/domain/Repository.go
@@ -2,15 +2,13 @@ package domain
 
 import (
 	expensesDomain "TravelAPI/pkg/expenses/domain"
-	scheduleDomain "TravelAPI/pkg/schedule/domain"
 )
 
 type Repository interface {
 	Get(travelID *string) (*Travel, error)
 	Gets(userID *string) (*[]Travel, error)
 	Post(travel *Travel) (*Travel, error)
-	PutSchedule(schedule *scheduleDomain.Schedule, travelID *string) error
-	PutCustomEntryLocations(customEntryLocations *[]string, travelID *string) error
+	Put(travel *Travel) (*Travel, error)
 	PutExpenses(userID, travelID *string, expense *expensesDomain.Expense, exists *bool) error
 	PutTravelUsers(userID, travelID *string, exists *bool) error
 	Delete(travelID *string) error
